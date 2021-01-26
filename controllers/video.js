6a017b8044e4cf45d56d45adce31765bc12f7a28
Riad Afridi Shibly <riadafridishibly@gmail.com>
2021-01-26 19:07:00 +0600

    Add thumnail getter and get content type from the files.json file

--- controllers/video.js
+++ controllers/video.js
@@ -11,8 +11,6 @@ const videoController = (req, res, next) => {
   if (!filesPath[videoID]) {
     res.status(404).send(`Video nof found for: ${videoID}`)
   } else {
-    // what if Video ID is invalid?
-    // Render Error page maybe?
     res.render('video', {
       title: filesPath[videoID].title,
       video_src: '/video/data/' + videoID,
@@ -21,15 +19,31 @@ const videoController = (req, res, next) => {
   }
 }
 
-// The follwoing part borrowed from this link...
+const getThumbnail = (req, res, next) => {
+  const videoID = req.params.video_id
+  if (!filesPath[videoID]) {
+    res.status(404).send(`Video nof found for: ${videoID}`)
+  } else {
+    // Should I do this this way?
+    const thumbnailPath = filesPath[videoID].thumbnail_path
+    const thumnailStream = fs.createReadStream(thumbnailPath)
+    thumnailStream.pipe(res)
+  }
+}
+
+// The follwoing part borrowed from this link and modified...
 // https://github.com/Abdisalan/blog-code-examples/blob/master/http-video-stream/index.js
 const videoData = (req, res, next) => {
   const range = req.headers.range
+  // html5 video send Range like this
+  // Range: abcde-
+  // here abcde is the start number
   if (!range) {
     res.status(400).send('Requires Range header')
   }
 
   const filePath = filesPath[req.params.video_id].path
+  const contentType = filesPath[req.params.video_id].video_type
   const videoPath = filePath
   const videoSize = fs.statSync(filePath).size
 
@@ -42,7 +56,7 @@ const videoData = (req, res, next) => {
     'Content-Range': `bytes ${start}-${end}/${videoSize}`,
     'Accept-Ranges': 'bytes',
     'Content-Length': contentLength,
-    'Content-Type': 'video/mp4'
+    'Content-Type': contentType
   }
   res.writeHead(206, headers)
 
